0fbc083b296ea89c8f8c1ffd255ed8867ecaa8fc
David Martínez <d.19mar@gmail.com>
2016-02-27 15:58:30 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DJ SKIP</title>
+    <link rel="stylesheet" type="text/css" href="web.css" media="screen">
+</head>
+<body background="Imagenes/bright-color-background-wallpaper_1400x875_87614.jpg">
+
+<div style="text-align: center">
+    <img style="width:400px;" src="Imagenes/HEATBEAT_logo.png" />
+</div>
+
+<h1>DJ SKIP</h1>
+
+<div style="text-align: center">
+    <img style="padding-left: 60px ;align-content:center; width:400px;" src="Imagenes/Borgore_NRFmagazine.png" />
+    <img style="padding-left: 60px ;align-content:center; width:400px;" src="Imagenes/concierto,-discoteca,-dj,-megafiesta,-luces-167213.jpg" />
+    <img style="padding-left: 60px ;align-content:center; width:400px;" src="Imagenes/Ajax_DJ.jpg" />
+</div>
+
+<h2>Sobre SKIP</h2>
+
+<p>
+    Nacido en 1990 en Vitoria-Gasteiz, DJ SKIP es actualmente uno de los DJs más famosos de Europa.
+</p>
+<p>
+    Comenzó a interesarse por la música a los 10 años y a los 12 empezó su carrera como DJ.
+    La mayoría de sus canciones están dentro de diferentes subgéneros de la música <strong>house</strong>,
+    entre los que destacan el <strong>Electro House</strong> y  el <strong>Progressive House</strong>.
+</p>
+<p>
+    Su primer éxito y su ascenso como DJ lo logró en 2007 cuando lanzó su primer álbum de la mano de la discográfica <em>Spinnin' records</em>.
+    Desde entonces ha lanzado 3 álbumes más en 2009, 2012 y 2016 respectivamente.
+</p>
+<p>
+    En 2011 tocó por primera vez en el festival belga <strong>Tomorrowland</strong>, habiendo repetido cada año. En 2012 se estrenó en el <strong>Ultra</strong> de Miami.
+</p>
+
+<h2 style="padding-top: 20px">Próximos eventos</h2>
+
+<table>
+    <caption style="background: lavender">Marzo 2016</caption>
+    <tr>
+        <th>FECHA</th>
+        <th>LUGAR</th>
+    </tr>
+
+    <tr>
+        <td>5 de marzo de 2016</td>
+        <td>Madrid (España)</td>
+    </tr>
+
+    <tr>
+        <td>11 de marzo de 2016</td>
+        <td>Amsterdam (Países Bajos)</td>
+    </tr>
+
+    <tr>
+        <td>13 de marzo de 2016</td>
+        <td>Nueva York (EEUU)</td>
+    </tr>
+
+    <tr>
+        <td>16 de marzo de 2016</td>
+        <td>Miami (EEUU)</td>
+    </tr>
+
+    <tr>
+        <td>20 de marzo de 2016</td>
+        <td>Berlín (Alemania)</td>
+    </tr>
+
+    <tr>
+        <td>26 de marzo de 2016</td>
+        <td>Sidney (Australia)</td>
+    </tr>
+</table>
+<table>
+    <caption style="background: lavender">Abril 2016</caption>
+    <tr>
+        <th>FECHA</th>
+        <th>LUGAR</th>
+    </tr>
+
+    <tr>
+        <td>1 de abril de 2016</td>
+        <td>Londres (Reino Unido)</td>
+    </tr>
+
+    <tr>
+        <td>7 de abril de 2016</td>
+        <td>Las Vegas (EEUU)</td>
+    </tr>
+
+    <tr>
+        <td>12 de Abril de 2016</td>
+        <td>Río de Janeiro (Brasil)</td>
+    </tr>
+
+    <tr>
+        <td>14 de abril de 2016</td>
+        <td>Buenos Aires (Argentina)</td>
+    </tr>
+
+    <tr>
+        <td>22 de abril de 2016</td>
+        <td>Barcelona (España)</td>
+    </tr>
+
+    <tr>
+        <td>25 de abril de 2016</td>
+        <td>Pekín (China)</td>
+    </tr>
+
+    <tr>
+        <td>28 de abril de 2016</td>
+        <td>Tokio (Japón)</td>
+    </tr>
+</table>
+
+
+</body>
+</html>
